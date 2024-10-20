6cb483277b536a4642ca78289b1d51f9f0399437
mustofakamal <mustofa.kamal@gmail.com>
2024-10-20 12:13:13 -0400

    added icon for list all

--- src/app/components/TopMenu.tsx
+++ src/app/components/TopMenu.tsx
@@ -1,36 +1,47 @@
 // TopMenu.tsx
 import React from 'react';
 
+import Image from 'next/image';
+
+
 interface TopMenuProps {
-  view: 'list' | 'analytics';
-  setView: React.Dispatch<React.SetStateAction<'list' | 'analytics'>>;
+    view: 'list' | 'analytics';
+    setView: React.Dispatch<React.SetStateAction<'list' | 'analytics'>>;
 }
 
 const TopMenu: React.FC<TopMenuProps> = ({ view, setView }) => {
-  return (
-    <div
-      style={{ height: '100px', width: '100%', backgroundColor: 'white', paddingLeft: '170px' }}
-      className="flex items-center justify-start p-4"
-    >
-      {/* Buttons */}
-      <button
-        onClick={() => setView('list')}
-        className={`p-2 ${
-          view === 'list' ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-700'
-        } rounded-md hover:bg-blue-600`}
-      >
-        List All
-      </button>
-      <button
-        onClick={() => setView('analytics')}
-        className={`p-2 ml-2 ${
-          view === 'analytics' ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-700'
-        } rounded-md hover:bg-blue-600`}
-      >
-        Analytics
-      </button>
-    </div>
-  );
+    return (
+        <div
+            style={{ height: '100px', width: '100%', backgroundColor: 'white', paddingLeft: '300px'}}
+            className="flex items-center justify-start p-4"
+        >
+            {/* Image acting as a button */}
+            <button
+                onClick={() => setView('list')}
+                className="focus:outline-none flex flex-col items-center group" style={{paddingTop:"16px"}}
+            >
+                <Image
+                    src="/images/IOU.jpeg"
+                    alt="IOU"
+                    title="IOU"
+                    width={50}
+                    height={50}
+                    className="object-contain rounded-3xl"
+                />
+                <span className="text-blue-500 font-bold group-hover:text-blue-700">IOU</span>
+            </button>
+
+            {/* Analytics Button */}
+            <button
+                onClick={() => setView('analytics')}
+                style={{ marginLeft: '100px' }}
+                className={`p-2 ${view === 'analytics' ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-700'
+                    } rounded-md hover:bg-blue-600`}
+            >
+                Analytics
+            </button>
+        </div>
+    );
 };
 
 export default TopMenu;
